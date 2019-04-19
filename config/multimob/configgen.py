8a313d80e149de2cc04f0c0f904f3f18f7cc9e6c
HQ <xxxfiredragonmcxxx@gmail.com>
2019-04-19 16:20:02 +0800

    stuff

--- config/multimob/configgen.py
+++ config/multimob/configgen.py
@@ -1,3 +1,7 @@
 f = open("input.txt", "r")
+file = open("output.txt","w+")
 for x in f:
-  print(x)
+	file.write(x[4:-1] + "#" + x)
+	print(x[3:])
+file.close()
+f.close()
---
+++ start.bat
@@ -0,0 +1,1 @@
+java -Xmx4G -Xms4G -jar forge-1.12.2-14.23.5.2807-universal.jar nogui pause
